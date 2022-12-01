657a4c0958015bc9c8cf7471dbba8e115917c556
vividfax <vividfax@gmail.com>
2022-12-01 18:34:54 +0000

    Update reflection

--- days/16.js
+++ days/16.js
@@ -17,9 +17,6 @@ class Sketch16 extends Sketch {
         this.complete = true;
         this.noLoop = true;
         this.noClear = true;
-
-
-        // this.extraCanvas = createGraphics(540, 540, WEBGL);
     }
 
     update() {
@@ -28,68 +25,61 @@ class Sketch16 extends Sketch {
 
     display() {
 
+        threeD.clear();
+        this.display3D();
+        this.displayCircles();
+        this.display3D();
+        this.displayCircles();
+        this.display3D();
+        this.displayCircles();
+        this.display3D();
+        this.displayCircles();
+        this.display3D();
+        this.displayCircles();
+        this.display3D();
+
+        push();
+        stroke(this.palette.light);
+        noFill();
+        strokeWeight(25);
+        ellipse(width/2, height/2, 240, 240);
+        pop();
+    }
+
+    display3D() {
+
         threeD.lights();
         threeD.stroke(this.palette.light);
         threeD.strokeWeight(50);
-        threeD.fill(this.palette.black);
         threeD.specularMaterial(250);
         threeD.shininess(20);
         threeD.sphere(440);
         threeD.sphere(500);
         threeD.strokeWeight(1);
+        threeD.noStroke();
         threeD.texture(canvas);
-        threeD.torus(190, 80);
-
-        // twoD.push();
-        // let imageSize = 180;
-        // twoD.clear();
-        // twoD.background(this.palette.dark);
-        // twoD.tint(this.palette.mid);
-        // twoD.blendMode(DODGE);
-
-        // for (let i = imageSize/2; i < width; i += imageSize) {
-        //     for (let j = imageSize/2; j <= height; j += imageSize) {
-
-        //         twoD.push();
-        //         twoD.translate(width/2, height/2);
-        //         twoD.imageMode(CENTER);
-        //         twoD.rectMode(CENTER);
-        //         twoD.translate(i, j);
-        //         twoD.rotate(random(360));
-
-        //         twoD.noStroke();
-        //         twoD.fill(this.palette.light);
-        //         twoD.image(threeD, 0, 0, imageSize, imageSize);
-
-        //         twoD.pop();
-        //     }
-        // }
-        // twoD.pop();
-
-        // twoD.tint(this.palette.white);
-
-        // for (let i = 0; i <= twoD.width; i += imageSize*2) {
-        //     for (let j = 0; j <= twoD.height; j += imageSize*2) {
-
-        //         twoD.push();
-        //         twoD.imageMode(CENTER);
-        //         twoD.rectMode(CENTER);
-        //         twoD.translate(i, j);
-        //         twoD.rotate(random(360));
-
-        //         twoD.noStroke();
-        //         twoD.fill(this.palette.light);
-        //         twoD.image(threeD, 0, 0, imageSize*2, imageSize*2);
-
-        //         twoD.pop();
-        //     }
-        // }
-
-        // threeD.texture(twoD);
-        // threeD.noStroke();
-        // threeD.sphere(100);
-
+        threeD.torus(280, 180);
 
         image(threeD, 0, 0);
+    }
+
+    displayCircles() {
+
+        noFill();
+
+        for (let i = 0; i < 500; i++) { //500
+
+            let x = random(width);
+            let y = random(height);
+            let r = random(5, 100)/2;
+
+            stroke(randomColour(this.palette));
+            ellipse(x, y, r*2);
+
+            if (x-r*2 < 0) ellipse(width+x, y, r*2);
+            if (x+r*2 > width) ellipse(-x, y, r*2);
+            if (y-r*2 < 0) ellipse(x, height+y, r*2);
+            if (y+r*2 > height) ellipse(x, -y, r*2);
+        }
     }
 }
